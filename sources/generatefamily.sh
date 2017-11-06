1276c7a2dc0514c6ec743507211216d13409fff2
weiweihuanghuang <wweeiihhuuaanngg@gmail.com>
2017-11-06 20:43:27 +0100

    v1.6, added new glyphs, fixed some glyphs
    - Updated font files that had blank x.sc and y.sc.
    - Removed using ttfautohint CTRL files from the build process as the TTFs are no longer the same ones being generated by the latest Glyphs App therefore the point references are  wrong.
    - Added Welsh glyphs /Ygrave/ygrave/Ycircumflex/ycircumflex/Wacute/wacute/Wgrave/wgrave/Wcircumflex/wcircumflex/Wdieresis/wdieresis/ygrave.sc/ycircumflex.sc/wacute.sc/wgrave.sc/wcircumflex.sc/wdieresis.sc
    - Added new arrows: /leftHookArrow/rightHookArrow/anticlockwiseOpenCircleArrow/clockwiseOpenCircleArrow

--- sources/generatefamily.sh
+++ sources/generatefamily.sh
@@ -8,7 +8,8 @@ for i in ExtraLight Thin; do
     echo "WorkSans-$i.ttf hinted"
 done
 for i in Light Regular Medium SemiBold Bold ExtraBold Black ; do
-    ttfautohint -n -w G -x 0 -f latn --control-file=hinting/WorkSans-$i.ctrl WorkSans-$i#1.ttf WorkSans-$i.ttf
+    # ttfautohint -n -w G -x 0 -f latn --control-file=hinting/WorkSans-$i.ctrl WorkSans-$i#1.ttf WorkSans-$i.ttf
+    ttfautohint -n -w G -x 0 -f latn WorkSans-$i#1.ttf WorkSans-$i.ttf
     echo "WorkSans-$i.ttf hinted"
 done
 
