5ca2532c30f0ace7a9d45f7f70705210b39379e9
weiweihuanghuang <wweeiihhuuaanngg@gmail.com>
2018-11-07 21:01:01 +1100

    Testing build

--- sources/build.sh
+++ sources/build.sh
@@ -4,19 +4,30 @@ glyphsSource="WorkSans.glyphs"
 VFname="WorkSans-VF"
 
 fontmake -o variable -g $glyphsSource
+echo "${VFname}.ttf generated"
 
-# mv variable_ttf/${VFname}.ttf ${VFname}.ttf
+mv variable_ttf/${VFname}.ttf ${VFname}.ttf
 
-# rm -rf master_ufo
-# rm -rf variable_ttf
+rm -rf master_ufo
+rm -rf instance_ufo
+rm -rf variable_ttf
 
 # python tools/swapBracketTrick.py ${VFname}.ttf "Upright"
 
-# gftools fix-nonhinting ${VFname}.ttf ${VFname}.ttf
-# gftools fix-dsig --autofix ${VFname}.ttf
-# gftools fix-gasp ${VFname}.ttf
+gftools fix-nonhinting ${VFname}.ttf ${VFname}.ttf
+gftools fix-dsig --autofix ${VFname}.ttf
+gftools fix-gasp ${VFname}.ttf
+echo "nonhinting, dsig, gasp fixed"
 
-# python tools/gftools-fix-vf-meta.py ${VFname}.ttf
+rm -rf ${VFname}-backup-fonttools-prep-gasp.ttf
 
-# ## move font into folder of dist/, with timestamp, then fontbake the font
+python tools/gftools-fix-vf-meta.py ${VFname}.ttf
+echo "vf-meta fixed"
+
+rm -rf ${VFname}.ttf
+mv ${VFname}.ttf.fix ${VFname}.ttf
+
+fontbakery check-googlefonts ${VFname}.ttf --ghmarkdown fontbakery-report.md
+
+## move font into folder of dist/, with timestamp, then fontbake the font
 # python3 tools/distdate-and-fontbake.py ${VFname}.ttf
