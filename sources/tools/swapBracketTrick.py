7af85679b906a53dd0bf3de6b8354bc4bffb2f5f
weiweihuanghuang <wweeiihhuuaanngg@gmail.com>
2018-11-08 10:25:24 +1100

    Update Build

--- sources/tools/swapBracketTrick.py
+++ sources/tools/swapBracketTrick.py
@@ -5,14 +5,12 @@ import sys
 import fontTools
 from fontTools.ttLib import TTFont
 from fontTools.varLib.featureVars import addFeatureVariations
-from fontTools.ttx import makeOutputFileName
 
-inputTTF = sys.argv[1]
-# style = sys.argv[2]
+inputTTF = sys.argv[-1]
+print(inputTTF)
 
-# f = TTFont(fontPath)
+
 f = TTFont(inputTTF)
-
 condSubst = [
     # A list of (Region, Substitution) tuples.
     ([{"wght": (0.158, 0.564)}], {"uniF8FF": "uniF8FF.001"}),
@@ -39,8 +37,5 @@ condSubst = [
 
 addFeatureVariations(f, condSubst)
 
-# newFontPath = fontPath.split(".")[0] + "-italic.ttf" 
-# f.save(fontPath)
-extension = os.path.splitext(inputTTF)[1]
-outputTTF = makeOutputFileName(inputTTF, '', extension)
+outputTTF = inputTTF.replace('.ttf', '-swap.ttf')
 f.save(outputTTF)
