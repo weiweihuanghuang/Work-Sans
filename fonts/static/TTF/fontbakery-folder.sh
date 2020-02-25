159bf35909e5c88d06a06bc842c1dfec49e8d225
weiweihuanghuang <wweeiihhuuaanngg@gmail.com>
2020-02-25 17:34:44 +1100

    - Generated new fonts

--- fonts/static/TTF/fontbakery-folder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in *.ttf; do
-    fontbakery check-googlefonts $i --ghmarkdown ${i}-fontbakery-report.md
-    # fontbakery check-googlefonts -c com.google.fonts/check/usweightclass -c com.google.fonts/check/name/familyname -c com.google.fonts/check/name/match_familyname_fullfont $i --ghmarkdown ${i}-fontbakery-report.md
-    echo "Processing $i ..."
-done
