aaa3779d0cc401372e0a5154d55362a4d9124e58
weiweihuanghuang <1271897+weiweihuanghuang@users.noreply.github.com>
2021-08-06 14:27:15 +1000

    Updated fonts to include autohinting Updated fonts to include cased glottal stop

---
+++ sources/tools/fontvalidator-folder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in *.ttf; do
+	echo "Processing $i ..."
+	# FontValidator -report-in-font-dir -no-raster-tests -only-tables +table 'glyf' -file $i
+	FontValidator -report-in-font-dir -no-raster-tests -file $i
+	# for i in *.html ; do mv $i Reports/${i} ; done
+	for i in *.xsl ; do rm $i ; done
+	for i in *.xml ; do rm $i ; done
+done
